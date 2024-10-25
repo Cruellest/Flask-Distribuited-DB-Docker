59e4f2b7a01261912fd2aa5fd6585e637230e180
Gabriel Paes <gabriel81523642@gmail.com>
2024-10-25 15:45:02 -0400

    Fixed some variables that should be constants

--- config/config.py
+++ config/config.py
@@ -14,13 +14,18 @@ class Config:
     @staticmethod
     
     def get_database_uri():
-        db_type = os.getenv('DB_TYPE','sqlite')
+        DB_TYPE = os.getenv('DB_TYPE','sqlite')
         
-        if db_type == "mysql":
+        if DB_TYPE == "mysql":
+            #Usuario MySQL
             MYSQL_USER = os.getenv('MYSQL_USER', 'root')
+            #Senha MySQL
             MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
+            #Endereco do Servidor MySQL
             MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
+            #Porta de Conexao ao MySQL
             MYSQL_PORT = os.getenv('MYSQL_PORT', '3306')
+            #Tipo de database MySQL
             MYSQL_DB = os.getenv('MYSQL_DATABASE', 'mydb')
             return f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
         
